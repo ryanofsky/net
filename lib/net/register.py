94d5e1ac6963a50ded7feb61a5c17492d71c0ba3
Russell Yanofsky <russ@yanofsky.org>
2005-07-10 16:58:41 +0000

    Add virtual server with mod_python redirects
    * net
    * net/__init__.py
    * net/register.py
        new python module with a mod_python handler
    
    * etc/apache2/conf/apache2.conf
        add virtual server
    
    * etc/apache2/conf/modules.d/47_mod_python.conf
        new file for mod_python configuration
    
    * etc/conf.d/apache2
        enable mod_python
    
    
    git-svn-id: https://russ.yanofsky.org/svn/net/trunk@17 2f804e4b-39fb-0310-8769-96c280e2130e

---
+++ lib/net/register.py
@@ -0,0 +1,15 @@
+from mod_python import apache, util
+import urllib
+
+def handler(req):
+  req.content_type = 'text/plain'
+  try:
+    host = req.headers_in['Host']
+  except KeyError:
+    url = ''
+  else:
+    url = 'http://%s%s' % (host, req.unparsed_uri)
+  
+  util.redirect(req, 'http://turtle/register.py'
+                + (url and '?url=' + urllib.quote(url, ':/')))
+  return apache.OK
