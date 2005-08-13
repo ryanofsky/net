bb38f66a9dbd2ea0e2b5ab2d01f823accf46f29e
Russell Yanofsky <russ@yanofsky.org>
2005-08-13 19:21:19 +0000

    Add headers to lib/net/register.py page to prevent caching. The caching behavior with both ie and mozilla still leaves me scratching my head. When you try to reload a page immediately after registration the browsers will try follow the cached redirect, but if you wait 10 or 15 seconds, then reloading the url will work.
    Actually that gives me an idea. Maybe the browser is comparing the
    "Expiration:" time against the local time instead of the "Date:"
    time, and the local time is around 10 seconds off...
    
    * lib/net/register.py
    
    
    git-svn-id: https://russ.yanofsky.org/svn/net/trunk@55 2f804e4b-39fb-0310-8769-96c280e2130e

--- lib/net/register.py
+++ lib/net/register.py
@@ -1,8 +1,9 @@
 from mod_python import apache, util
+
 import urllib
+import rfc822
 
 def handler(req):
-  req.content_type = 'text/plain'
   try:
     host = req.headers_in['Host']
   except KeyError:
@@ -10,6 +11,18 @@ def handler(req):
   else:
     url = 'http://%s%s' % (host, req.unparsed_uri)
 
-  util.redirect(req, 'http://kalam/register.py'
-                + (url and '?url=' + urllib.quote(url, ':/')))
+  loc = 'http://kalam/register.py' + (url and '?url=' + urllib.quote(url, ':/'))
+  date = rfc822.formatdate()
+
+  req.status = apache.HTTP_MOVED_TEMPORARILY
+  req.headers_out['Location'] = loc
+  #req.headers_out['Refresh'] = '0; %s' % loc
+  req.headers_out['Cache-Control'] = 'no-store, no-cache'
+  req.headers_out['Pragma'] = 'no-cache'
+  req.headers_out['Last-Modified'] = date
+  req.headers_out['Expires'] = date
+  req.headers_out['Date'] = date
+  req.content_type = 'text/html'
+  req.write('<p>The document has moved <a href="%s">here</a></p>\n' % loc)
+
   return apache.OK
