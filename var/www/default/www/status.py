0a00487cd4b9606cf2f76614689ea82f78a0f457
Russell Yanofsky <russ@yanofsky.org>
2005-08-13 19:31:56 +0000

    Add cheesy status script to website
    * var/www/default/www/status.py
    
    
    git-svn-id: https://russ.yanofsky.org/svn/net/trunk@59 2f804e4b-39fb-0310-8769-96c280e2130e

---
+++ var/www/default/www/status.py
@@ -0,0 +1,68 @@
+page = """<html>
+<head><title>Internet Status Page</title></head>
+<body>
+<h2>Internet Status Page</h2>
+<table border=1>
+  <tr>
+    <td><strong>Internet Gateway</strong></td>
+    <td><font color=green>Up</font></td>
+  </tr>
+  <tr>
+    <td><strong>Satellite Link</strong></td>
+    <td>[if-any sat]<font color=green>Up</font>[else]<font color=red>Down</font>[end]</td>
+  </tr>
+</table>
+<p><a href="/">Back to Main Page<a></p>
+<hr>
+<div><small>Generated <i>[date]</i><br>
+Note: more detailed connection statistics will be added soon.</small></div>
+</body>
+</html>
+"""
+
+import cgi
+import cStringIO
+import time
+import socket
+import select
+import errno
+import time
+
+from net import ezt, addr, db, hosts
+
+SAT_ADDR = ('203.110.192.17', 23)
+TIMEOUT = 1.0
+
+def connect_sat():
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  try:
+    sock.setblocking(0)
+    timeout = time.time() + TIMEOUT
+    while True:
+      result = sock.connect_ex(SAT_ADDR)
+      if result in (0, errno.EISCONN):
+        return 1
+      if result in (errno.EINPROGRESS, errno.EALREADY, errno.EWOULDBLOCK):
+        readable, writable, special = select.select([], [sock], [], 
+                                                    max(0, timeout - time.time()))
+        if time.time() > timeout:
+          return None
+        continue
+      return None
+  finally:
+    sock.close()
+
+def index(req, outgoing='', sort=''):
+  sat = connect_sat()
+  vars = {
+    'date': time.strftime('%a %b %d %H:%M:%S %Z %Y'),
+    'sat': sat,
+  }
+
+  out = cStringIO.StringIO()
+  template = ezt.Template()
+  template.parse(page)
+  template.generate(out, vars)
+  return out.getvalue()
+
+
