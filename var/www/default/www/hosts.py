b116b0a80b8e5a7f9155b18a16cec52bf0f166a7
Russell Yanofsky <russ@yanofsky.org>
2005-08-15 19:27:04 +0000

    Add ip address to host listing
    * var/www/default/www/hosts.py
    
    
    git-svn-id: https://russ.yanofsky.org/svn/net/trunk@68 2f804e4b-39fb-0310-8769-96c280e2130e

--- var/www/default/www/hosts.py
+++ var/www/default/www/hosts.py
@@ -5,7 +5,8 @@ def index():
   try:
     cursor = conn.cursor()
     try:
-      return ''.join(['%s %s\n' % (host.name, addr.mac_str(host.mac))
+      return ''.join(['%s %s (%s)\n' % (host.name, addr.mac_str(host.mac),
+                                      addr.ip_str(host.ip))
                       for host in hosts.get_hosts(cursor)])
     finally:
       cursor.close()
