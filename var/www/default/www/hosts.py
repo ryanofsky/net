bb036efe39c9de2a64281cd5815f79536a599ac1
Russell Yanofsky <russ@yanofsky.org>
2005-08-31 20:27:18 +0000

    Add spacing to hosts list
    git-svn-id: https://russ.yanofsky.org/svn/net/trunk@77 2f804e4b-39fb-0310-8769-96c280e2130e

--- var/www/default/www/hosts.py
+++ var/www/default/www/hosts.py
@@ -5,7 +5,7 @@ def index():
   try:
     cursor = conn.cursor()
     try:
-      return ''.join(['%s %s (%s)\n' % (host.name, addr.mac_str(host.mac),
+      return ''.join(['%-15s %s  %s\n' % (host.name, addr.mac_str(host.mac),
                                       addr.ip_str(host.ip))
                       for host in hosts.get_hosts(cursor)])
     finally:
