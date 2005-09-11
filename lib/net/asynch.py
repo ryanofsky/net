57967c8baec2dcd097c047443eed636cdbe9cf65
Russell Yanofsky <russ@yanofsky.org>
2005-09-11 22:26:54 +0000

    Hopefully get yield semantics right when waiting for one operation OR another. I haven't been able to test this functionality yet, but I'll need it to implement timeout operations.
    * lib/net/asynch.py
      (AsynchOp.add_hook, AsynchOp._call_hooks,
       AsynchOp.set_hook, AsynchOp._call_hook):
        replace first pair for functions with second, simplifing hook mechanism
        so it's only possible to specify one completion hook instead of many
    
      (Fiber.run):
        unrelated changes to docstring
    
      (Fiber._run, Fiber._add_hooks):
        move _add_hooks functionality into _run and cause hooks that wake up
        the fiber to delete all other hooks that could wake up the fiber so
    
    
    
    git-svn-id: https://russ.yanofsky.org/svn/net/trunk@96 2f804e4b-39fb-0310-8769-96c280e2130e

--- lib/net/asynch.py
+++ lib/net/asynch.py
@@ -9,7 +9,7 @@ import time
 class AsynchOp:
   def __init__(self, *args, **kwargs):
     """Create instance and call start() if given arguments"""
-    self._hooks = []
+    self._hook = None
     if args or kwargs:
       self.start(*args, **kwargs)
 
@@ -30,17 +30,18 @@ class AsynchOp:
     Should return False to stop recieving notifications, True otherwise"""
     raise NotImplementedError
 
-  def add_hook(self, hook):
+  def set_hook(self, hook):
     """Set callback to invoke when asynchronous operation completes"""
     assert not self.done()
-    assert callable(hook)
-    self._hooks.append(hook)
+    assert ((callable(hook) and self._hook is None)
+            or hook is None and callable(self._hook))
+    self._hook = hook
 
-  def _call_hooks(self):
+  def _call_hook(self):
     """Call hook functions, if present"""
     assert self.done()
-    for hook in self._hooks:
-      hook()
+    if self._hook is not None:
+      self._hook()
 
 
 class ConnectOp(AsynchOp):
@@ -53,7 +54,7 @@ class ConnectOp(AsynchOp):
       self.error = None
 
     if self.done():
-      self._call_hooks()
+      self._call_hook()
     else:
       reactor._register_writable(self, sock)
 
@@ -71,7 +72,7 @@ class ConnectOp(AsynchOp):
     self.error = sock.getsockopt(socket.SOL_SOCKET, socket.SO_ERROR)
 
     if self.done():
-      self._call_hooks()
+      self._call_hook()
       return False
     else:
       return True
@@ -99,7 +100,7 @@ class RecvOp(AsynchOp):
         self.error = why
 
     assert self.done()
-    self._call_hooks()
+    self._call_hook()
     return False
 
 
@@ -125,7 +126,7 @@ class SendOp(AsynchOp):
         self.error = why
 
     assert self.done()
-    self._call_hooks()
+    self._call_hook()
     return False
 
 
@@ -198,50 +199,84 @@ class Fiber(AsynchOp):
     The generator can yield operations or sequences of operations and it
     will be automatically resumed when the operations complete. If it
     yields a sequence of operations, it will be resumed when ANY ONE of the
-    operations completes. If it yields a sequence of subsequences of
-    operations, it will resume when ALL of the operations in ANY ONE of the
-    subsequences completes.
+    operations in the sequence completes. If it yields a sequence of
+    subsequences of operations, it will resume when ALL of the operations in
+    ANY ONE of the subsequences completes.
 
     Examples:
-      # wait for read operation OR timeout to complete
+      # automatically resume generator when read operation completes
+      yield read
+
+      # resume when read operation OR timeout operation completes
       yield (read, timeout)
 
-      # wait for read AND write operations, OR timeout operation
+      # resume when read AND write operations, OR timeout operation completes
       yield ((read, write), timeout)
-    
-      # wait for read AND write operations to complete
+
+      # resume when read AND write operations completes
       yield ((read, write),)
 
     In this way, the resume mechanism is fully general and a fiber can
-    for any combination of operations to complete.
-    """
+    for any combination of operations to complete."""
     raise NotImplementedError
 
   def _run(self, *arg, **kwargs):
-    """Wrapper around user-defined run() that cleans up afterwards"""
+    """Wrapper around run() that handles yield values and calls hook"""
     for op_sets in self.run(*arg, **kwargs):
-      if self._add_hooks(op_sets):
-        yield None
-    # Explicitly set generator to None in case any of the hooks want to 
-    # call done()
-    self.generator = None
-    self._call_hooks()
+      # Get fiber to automatically resume when yielded operations complete
 
-  def _add_hooks(self, op_sets):
-    """Add hooks to resume Fiber when specified operations complete"""
-    for op_set in isinstance(op_sets, AsynchOp) and (op_sets,) or op_sets:
-      wait_all = []
-      for op in isinstance(op_set, AsynchOp) and (op_set,) or op_set:
-        if not op.done():
-          wait_all.append(op)
-          def hook(wait_all=wait_all, op=op):
-            wait_all.remove(op)
-            if not wait_all:
-              self._kick()
-          op.add_hook(hook)
-      if not wait_all:
-        return False
-    return True
+      # if yield value is not a sequence, make it one
+      if isinstance(op_sets, AsynchOp) or op_sets is None:
+        op_sets = op_sets,
+
+      # list of sublists of operations. operations remove themselves from
+      # sublists as they complete and fiber resumes when ALL the operations
+      # in ANY of the sublists are gone
+      wait_any = []
+
+      for op_set in op_sets:
+        # if yielded sequence element is not a subsequence, make it one
+        if isinstance(op_set, AsynchOp) or op_set is None:
+          op_set = op_set,
+
+        # sublist of operations. if empty, fiber resumes
+        wait_all = []
+
+        for op in op_set:
+          if not op.done():
+            # hook function called when op completes
+            def hook(wait_any=wait_any, wait_all=wait_all, op=op, fiber=self):
+              wait_all.remove(op)
+
+              # if nothing left to wait for, remove any hooks and wake up fiber
+              if not wait_all:
+                for ops in wait_any:
+                  for op in ops:
+                    op.set_hook(None)
+                fiber._kick()
+
+            op.set_hook(hook)
+            wait_all.append(op)
+
+        # if set is empty, remove any hooks and resume fiber right away
+        if wait_all:
+          wait_any.append(wait_all)
+        else:
+          for ops in wait_any:
+            for op in ops:
+              op.set_hook(None)
+          del wait_any[:]
+          break
+
+      # Go to sleep, will wake up when one of the operations the fiber
+      # is waiting on completes and calls kick()
+      if wait_any:
+        yield None
+
+    # Fiber has finished, call hooks, but first set generator to None in case
+    # any of the hooks want to fiber.done()
+    self.generator = None
+    self._call_hook()
 
   def done(self):
     return not self.generator
